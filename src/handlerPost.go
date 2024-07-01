d7326f05cbe39618d89d275bf9ce5ace4465637b
Arnauddddddd <arnaudpf2005@gmail.com>
2024-07-01 15:04:10 +0200

    search user

--- src/handlerPost.go
+++ src/handlerPost.go
@@ -168,7 +168,7 @@ func (E *Engine) SubmitChangeUsername(c *fiber.Ctx) error {
 		return nil
 	}
 	E.CurrentData.ErrorMsg = "Please provide a valid name"
-	c.Redirect("/edit_profil")
+	c.Redirect("/")
 	return nil
 }
 
@@ -178,7 +178,7 @@ func (E *Engine) SubmitChangePictureProfile(c *fiber.Ctx) error {
 	E.GetCookieUser(c)
 	E.CurrentData.User.ProfilePicture = picture
 	E.ExecuteSQL("UPDATE users SET profile_picture = '" + picture + "' WHERE id = " + strconv.Itoa(E.CurrentData.User.Id) + ";")
-	c.Redirect("/view_profil")
+	c.Redirect("/")
 	return nil
 }
 
